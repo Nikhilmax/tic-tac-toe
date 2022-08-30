79291e85ba48413eae4dee339d64720acc494baa
Vecha Nikhil <67752936+Nikhilmax@users.noreply.github.com>
2022-08-30 08:12:42 +0530

    Add files via upload

---
+++ tic.js
@@ -0,0 +1,58 @@
+let curr = "X"
+function game(){
+    var id1 = document.querySelector("#td1")
+    var id2 = document.querySelector("#td2")
+    var id3 = document.querySelector("#td3")
+    var id4 = document.querySelector("#td4")
+    var id5 = document.querySelector("#td5")
+    var id6 = document.querySelector("#td6")
+    var id7 = document.querySelector("#td7")
+    var id8 = document.querySelector("#td8")
+    var id9 = document.querySelector("#td9")
+
+    id1.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+
+    id2.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+
+    id3.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+
+    id4.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+
+    id5.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+
+    id6.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+
+    id7.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+
+    id8.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+
+    id9.addEventListener("click",function(){
+        id1.textContent = (curr=="X" ?"O":"X")
+        curr = (curr=="X" ?"O":"X")
+    })
+}
+
