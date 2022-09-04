7be54f7d10c3e67205ddaaf2a5babf8bee06a284
Vecha Nikhil <67752936+Nikhilmax@users.noreply.github.com>
2022-09-04 23:26:58 +0530

    Update tic.js

--- tic.js
+++ tic.js
@@ -1,58 +1,74 @@
-let curr = "X"
-function game(){
-    var id1 = document.querySelector("#td1")
-    var id2 = document.querySelector("#td2")
-    var id3 = document.querySelector("#td3")
-    var id4 = document.querySelector("#td4")
-    var id5 = document.querySelector("#td5")
-    var id6 = document.querySelector("#td6")
-    var id7 = document.querySelector("#td7")
-    var id8 = document.querySelector("#td8")
-    var id9 = document.querySelector("#td9")
+let winnerb = document.getElementById('winner')
+let resetbtn = document.getElementById('resetbtn')
+let boxes = Array.from(document.getElementsByClassName('box'))
 
-    id1.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
+let winner_i = getComputedStyle(document.body).getPropertyValue('--winning_blocks')
+const o = "O"
+const x = "X"
+let current_player = x
 
-    id2.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
+let spaces = Array(9).fill(null)
 
-    id3.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
-
-    id4.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
-
-    id5.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
-
-    id6.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
-
-    id7.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
-
-    id8.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
-
-    id9.addEventListener("click",function(){
-        id1.textContent = (curr=="X" ?"O":"X")
-        curr = (curr=="X" ?"O":"X")
-    })
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click',bc))
 }
 
+function bc(e){
+    const id = e.target.id
+
+    if(!spaces[id]){
+        spaces[id] = current_player
+        e.target.innerText = current_player
+        
+        if(playerhaswon() !== false){
+            
+            let winnig_blocks = playerhaswon()
+            winnig_blocks.map(box => boxes[box].style.backgroundColor=winner_i)
+            if(current_player==x){
+                winnerb.innerHTML="X has won"
+            }
+            else{
+                winnerb.innerHTML="O has won"
+            }
+            reset()
+            return 
+        }
+        current_player= current_player == x ? o:x
+    }
+}
+
+const winning_combos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+function playerhaswon(){
+    for (const condition of winning_combos) {
+        let [a,b,c] = condition
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a]==spaces[c])){
+            return [a,b,c]
+        }
+    }
+    return false
+}
+resetbtn.addEventListener('click',reset)
+
+function reset(){
+    spaces.fill(null)
+
+    boxes.forEach(box => {
+        box.innerText=''
+        box.style.backgroundColor=''
+    })
+
+    current_player = x
+}
+
+
+startGame()
